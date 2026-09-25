82c954a4b6561b0270ac754b04314b9eb2cab749
Anju-7 <anjanathanigaiselvan@gmail.com>
2026-09-25 14:53:09 +0530

    main upt

--- main.py
+++ main.py
@@ -25,7 +25,9 @@ else:
     ssl._create_default_https_context = _create_unverified_https_context
 
 nltk.download("punkt", quiet=True)
+nltk.download("punkt_tab", quiet=True)
 nltk.download("stopwords", quiet=True)
+
 
 app = FastAPI()
 
